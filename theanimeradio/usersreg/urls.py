e345e43f3dd713bcf1d438fbcf3280c16d983aae
soroush javed sulehri <saroushjaved@gmail.com>
2020-08-15 13:13:52 -0400

    added form registration with the backend complete

--- theanimeradio/usersreg/urls.py
+++ theanimeradio/usersreg/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.signup, name="signup"),   
+    path('signup', views.signup, name="signup"),  
 ]
--- theanimeradio/usersreg/views.py
+++ theanimeradio/usersreg/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password1 = request.POST['password1']
+        username=request.POST['username']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if len(password1) >= 8: 
+            if password1 != password2:
+                messages.info(request, " Password must be 8 Character Long " )
+                return redirect('/usersreg/signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "Email is already taken" )
+                return redirect('/usersreg/signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "username is already taken" )
+                return redirect('/usersreg/signup')
+            
+            else:
+                user = User.objects.create_user( username=username, first_name=first_name , last_name=last_name , password=password1)
+                user.save()
+                messages.info(request, "Registration Successful" )
+                return redirect('/usersreg/signup')
+        else:
+            messages.info(request, " Password must be 8 Character Long " )
+            return redirect('/usersreg/signup')
+    
+    else:
+        return render(request, 'signup.html')
