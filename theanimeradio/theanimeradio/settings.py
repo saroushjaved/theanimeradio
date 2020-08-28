7e8469a709985dc395409a0072a16ab66098ecfc
soroush javed sulehri <saroushjaved@gmail.com>
2020-08-28 08:15:31 -0400

    DB update

--- theanimeradio/theanimeradio/settings.py
+++ theanimeradio/theanimeradio/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'theanimeradio',
         'USER': 'soroush',
-        'PASSWORD': 'amharmah26',
+        'PASSWORD': 'amharmah2646',
         'HOST': 'localhost',
         'PORT': '',
     }
